0646bae9d35e265b39be41c49e06ded4e8722f21
omple <55158761+omple@users.noreply.github.com>
2020-04-21 18:02:56 -0400

    Added existing code to Github
    -added code from local storage to the github

---
+++ Simple Weather App/src/com/company/Main.java
@@ -0,0 +1,36 @@
+package com.company;
+
+import com.google.gson.*;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception{
+        String apiKey = "105e15195f64a14d618ca02d48c5e213";
+        String cityID = "1006984"; //East London
+
+        //String urlString = "api.openweathermap.org/data/2.5/weather?id=" + cityID + "&appid=" + apiKey;
+
+        //testing with ISS API to test out API code
+        String urlString = "http://api.open-notify.org/iss-now.json";
+        System.out.println(urlString);
+
+        // Connect to the URL using java's native library
+        URL url = new URL(urlString);
+        URLConnection request = url.openConnection();
+        request.connect();
+
+        // Convert to a JSON object to print data
+        JsonParser jp = new JsonParser(); //from gson
+        JsonElement root = jp.parse(new InputStreamReader((InputStream) request.getContent())); //Convert the input stream to a json element
+        JsonObject rootobj = root.getAsJsonObject(); //May be an array, may be an object.
+        String country = rootobj.get("message").getAsString(); //just grab the zipcode
+
+        System.out.println(country);
+
+        //code to connect to url and code to convert json object to data from users Ad Infinitum and user2654569, https://stackoverflow.com/a/21964051
+    }
+}
